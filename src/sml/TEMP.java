35d21310f6c02e6849ef9ff4f015f29babb2a918
JoolsF <joolsmf@gmail.com>
2015-02-02 15:53:22 +0000

    Solved problem of creating arguments dynamically and passing them in an array

--- src/sml/TEMP.java
+++ src/sml/TEMP.java
@@ -14,7 +14,7 @@ public class TEMP {
 	
 	private void launch(){
 		TranslatorTest t = new TranslatorTest();
-		System.out.println(t.getInstruction("div"));
+		t.getInstruction("div");
 		
 	}
 
@@ -22,47 +22,31 @@ public class TEMP {
 
 class TranslatorTest {
 	
-	public Instruction getInstruction(String label){
+	public void getInstruction(String label){
 		String classPrefix= "sml.";
 		String classSuffix = "Instruction";
 		String fullLabel = classPrefix + label.substring(0,1).toUpperCase() + label.substring(1,label.length()) + classSuffix ;
 		
+		Class<?> targetClass = null;
+		Constructor<?>[] all = targetClass.getConstructors();
+		Constructor<?> constructor = all[1];
 		
-		Class<?> c = null;
-		Object obj = null;
-		Constructor<?> cons = null;
 		
+		Instruction instruction = null;
+		
+		
+				
 		try {
-			c = Class.forName(fullLabel);
-			//obj = c.newInstance();
-		
-			Constructor<?>[] a = c.getConstructors();
-			//THIS NEEDS CHANGING, ASSUMES THAT CORRECT CONSTUCTOR IS ALWAYS SECOND ONE
-			Parameter [] param = a[1].getParameters();
-			
-			//get correct constructor.  All constructors start with a string  args and have between 1 and 4 int args
-			if(param.length == 2){
-				cons = c.getConstructor(new Class[]{String.class, int.class});
-				obj = cons.newInstance(new Object[] { label, 1});
-			} else if(param.length == 3){
-				cons = c.getConstructor(new Class[]{String.class, int.class, int.class});
-				obj = cons.newInstance(new Object[] { label, 1, 1});
-			} else if(param.length == 4){
-				cons = c.getConstructor(new Class[]{String.class, int.class, int.class, int.class});
-				obj = cons.newInstance(new Object[] { label, 1, 1, 1});
-			} else{
-				cons = c.getConstructor(new Class[]{String.class, int.class, int.class, int.class, int.class});
-				obj = cons.newInstance(new Object[] { label, 1, 1, 1, 1});
-			}
-			
-			
-			
-		} catch (ClassNotFoundException | NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			targetClass = Class.forName(fullLabel);
+		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		return (Instruction) obj;
-	}
-}
+		
+		
 
+	}
+	
+	
+}
