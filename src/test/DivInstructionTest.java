ad44b4cdd8842d0dfb45fd880ec8bc087a536953
joolsmf <joolsmf@gmail.com>
2015-02-09 17:30:10 +0000

    Unit testing underway.  Not done first as didn't know about this requirement beforehand.

---
+++ src/test/DivInstructionTest.java
@@ -0,0 +1,57 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import sml.DivInstruction;
+import sml.Machine;
+import sml.Registers;
+
+
+public class DivInstructionTest {
+
+	Machine testMachine;
+	Registers testResisters;
+
+	String label;
+	int resultRegister;
+	int op1;
+	int op2;
+	
+	int value1;
+	int value2;	
+	
+	@Before
+	public void setUp() throws Exception {
+		testMachine = new Machine();
+		testResisters = new Registers();
+		testMachine.setRegisters(testResisters);
+		label = "f1";
+		resultRegister = 0;
+		op1 = 1;
+		op2 = 2;
+		
+		value1 = 25;
+		value2 = 5;
+		
+		testMachine.getRegisters().setRegister(op1, value1);
+		testMachine.getRegisters().setRegister(op2, value2);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void testExecute() {
+		DivInstruction test = new DivInstruction(label, resultRegister, op1, op2);
+		test.execute(testMachine);
+		int expected = value1 / value2;
+		int output = testMachine.registers.getRegister(resultRegister);
+		assertEquals(expected, output);
+	}
+
+}
---
+++ src/test/LinInstuctionTest.java
@@ -0,0 +1,54 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import sml.LinInstruction;
+import sml.Machine;
+import sml.Registers;
+
+public class LinInstuctionTest {
+	Machine testMachine;
+	Registers testResisters;
+	String label;
+	
+	int register;
+	int value;
+		
+	
+	@Before
+	public void setUp() throws Exception {
+		testMachine = new Machine();
+		testResisters = new Registers();
+		testMachine.setRegisters(testResisters);
+		label = "f1"; 
+		register = 7;
+		value = 5;
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testExecute() {
+		LinInstruction test = new LinInstruction(label, register, value);
+		test.execute(testMachine);
+        int expectedValue = testMachine.registers.getRegister(register);
+		assertEquals(value, expectedValue );
+		
+	}
+	
+	@Test
+	public void testToString(){
+		LinInstruction test = new LinInstruction(label, register, value);
+		String output = test.toString();
+		String expected = "f1: lin register " + register + " value is " + value;
+		assertEquals(output,expected);
+	}
+
+}
